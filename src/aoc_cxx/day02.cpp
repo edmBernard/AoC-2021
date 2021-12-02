3af5f356a317e36c3050a6ee04a7acc8b199ba8e
edmbernard <bernard.erwan@gmail.com>
2021-12-02 07:49:45 +0100

    day02: add part1

--- src/aoc_cxx/day02.cpp
+++ src/aoc_cxx/day02.cpp
@@ -5,12 +5,92 @@
 #include <spdlog/spdlog.h>
 
 #include <filesystem>
+#include <fstream>
+#include <charconv>
 
 namespace aoc {
 
 namespace fs = std::filesystem;
 
-RegisterCommand day02("day02,rust", {{"day02.txt", 1, 2}}, [](fs::path filename) -> std::tuple<uint64_t, uint64_t> {
+enum Direction {
+  Up,
+  Down,
+  Forward
+};
+
+RegisterCommand day02("day02", {
+    { "input_day02.txt",       2117664,   1275},
+    { "input_day02_test1.txt", 150,   5},
+  }, [](fs::path filename) -> std::tuple<uint64_t, uint64_t> {
+
+    std::ifstream infile(filename);
+    if (!infile.is_open()) {
+      throw std::runtime_error(fmt::format("File Not Found : {}", filename.string()));
+    }
+
+    std::vector<std::pair<Direction, uint16_t>> inputPuzzle;
+    std::string line;
+    while (getline(infile, line)) {
+
+      const auto match = line.find(" ");
+      if (match == std::string::npos) {
+        throw std::runtime_error(fmt::format("Fail to parse : {}", line));
+      }
+
+      std::string_view direction(line.data(), match);
+      std::string_view value(line.data() + match);
+
+      uint16_t result;
+      // +1 to escape matching space to remains in value
+      const auto [ptr, ec] = std::from_chars(line.data() + match + 1, line.data() + line.size(), result);
+      if (ec != std::errc())
+        throw std::runtime_error(fmt::format("Fail to parse : {}", line));
+
+      if (direction == "forward") {
+        inputPuzzle.emplace_back(Direction::Forward, result);
+      } else if (direction == "up") {
+        inputPuzzle.emplace_back(Direction::Up, result);
+      } else if (direction == "down") {
+        inputPuzzle.emplace_back(Direction::Down, result);
+      } else {
+        throw std::runtime_error(fmt::format("Unrecognize direction : {}", direction));
+      }
+    }
+
+    // part1
+    uint64_t position = 0;
+    uint64_t depth = 0;
+    for (auto& [dir, value] : inputPuzzle) {
+      switch (dir)
+      {
+      case Direction::Forward:
+        position += value;
+        break;
+      case Direction::Up:
+        depth -= value;
+        break;
+      case Direction::Down:
+        depth += value;
+        break;
+      }
+    }
+    spdlog::debug("depth={} position={}", depth, position);
+    uint64_t part1Result = depth * position;
+
+    // part2
+    uint16_t part2Result = 0;
+    // uint16_t previous = depthList[0] + depthList[1] + depthList[2];
+    // for (int idx = 2; idx < depthList.size(); ++idx) {
+    //   const uint16_t sum = depthList[idx] + depthList[idx-1] + depthList[idx-2];
+    //   if (sum > previous) {
+    //     ++part2Result;
+    //   }
+    //   previous = sum;
+    // }
+    return {part1Result, part2Result};
+});
+
+RegisterCommand day02rust("day02,rust", {{"day02.txt", 1, 2}}, [](fs::path filename) -> std::tuple<uint64_t, uint64_t> {
   uint64_t part1;
   uint64_t part2;
   rust::day02(filename.string(), part1, part2);
