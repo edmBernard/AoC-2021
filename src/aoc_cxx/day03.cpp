855a94020045ab31bc23f42c32185085a2ac4479
edmbernard <bernard.erwan@gmail.com>
2021-12-03 16:37:16 +0100

    day03: fix part2

--- src/aoc_cxx/day03.cpp
+++ src/aoc_cxx/day03.cpp
@@ -18,25 +18,8 @@ namespace rs = ranges;
 namespace rv = ranges::views;
 
 
-// std::vector<uint64_t> histogram(const std::vector<uint16_t>& inputPuzzle, uint64_t bit, uint64_t lineLength) {
-//   std::vector<uint64_t> occurenceOfBit(lineLength, 0);
-//   for (size_t i = 0; i < lineLength; ++i) {
-//     const uint64_t cursor = 1 << i;
-//     if (
-//     occurenceOfBit[occurenceOfBit.size()-i-1] +=
-//     for (auto& number : inputPuzzle) {
-//       if (line[i] == '0') {
-//         occurenceOf0[i] += 1;
-//       } else {
-//         occurenceOf1[i] += 1;
-//       }
-//     }
-//   }
-//   return occurenceOfBit;
-// }
-
 RegisterCommand day03("day03", {
-    { "input_day03.txt",       3429254,   5388270},
+    { "input_day03.txt",       3429254,   5410338},
     { "input_day03_test1.txt", 198,       230},
   }, [](fs::path filename) -> std::tuple<uint64_t, uint64_t> {
 
@@ -46,12 +29,14 @@ RegisterCommand day03("day03", {
     }
 
     std::vector<uint16_t> inputPuzzle;
+    std::vector<std::string> inputPuzzleCheat;
     std::vector<uint64_t> occurenceOf0;
     std::vector<uint64_t> occurenceOf1;
     std::string line;
     size_t lineLength = 0;
     while (getline(infile, line)) {
 
+      inputPuzzleCheat.push_back(line);
       uint16_t result;
       const auto [ptr, ec] = std::from_chars(line.data(), line.data() + line.size(), result, 2);
       if (ec != std::errc())
@@ -82,14 +67,11 @@ RegisterCommand day03("day03", {
     }
     const uint64_t part1Result = gammaRate * epsilonRate;
 
-
     uint64_t oxygen = 0;
     uint64_t co2 = 0;
 
+    // We sort to create a tree-like with 0 and 1
     std::sort(inputPuzzle.begin(), inputPuzzle.end());
-    for (auto& v : inputPuzzle) {
-      spdlog::debug("line={:#07b}", v);
-    }
 
     {
       auto begin = inputPuzzle.begin();
@@ -97,8 +79,8 @@ RegisterCommand day03("day03", {
       for (size_t i = 0, shift = lineLength -1; i < lineLength; ++i, --shift) {
         auto pos = std::find_if(begin, end, [&](auto value) { return (value >> shift) & 1; });
         if (pos != end) {
-          spdlog::debug("first={:#07b} last={:#07b} size={} pos={:#07b} ({}) iteration={}", *begin, *(end-1), end - begin, *pos, *pos, i);
-          if (pos - begin < end - pos) {
+          spdlog::debug("first={:#014b} last={:#014b} size={:04d} pos={:#014b} ({:04d}) pos-1={:#014b} iteration={}", *begin, *(end-1), end - begin, *pos, *pos, *(pos-1), i);
+          if (pos - begin <= end - pos) {
             begin = pos;
           } else {
             end = pos;
@@ -114,7 +96,7 @@ RegisterCommand day03("day03", {
       for (size_t i = 0, shift = lineLength -1; i < lineLength; ++i, --shift) {
         auto pos = std::find_if(begin, end, [&](auto value) { return (value >> shift) & 1; });
         if (pos != end) {
-          spdlog::debug("first={:#07b} last={:#07b} size={} pos={:#07b} ({}) iteration={}", *begin, *(end-1), end - begin, *pos, *pos, i);
+          spdlog::debug("first={:#014b} last={:#014b} size={:04d} pos={:#014b} ({:04d}) iteration={}", *begin, *(end-1), end - begin, *pos, *pos, i);
           if (pos - begin <= end - pos) {
             end = pos;
           } else {
@@ -124,6 +106,7 @@ RegisterCommand day03("day03", {
         }
       }
     }
+    spdlog::debug("oxygen={} co2={}", oxygen, co2);
 
     const uint64_t part2Result = oxygen * co2;
 
