b162947f57a8caa53ba8697c2797b215c49414b9
edmbernard <bernard.erwan@gmail.com>
2021-12-03 23:11:58 +0100

    day03: clean rust solution

--- src/aoc_rust/src/day03.rs
+++ src/aoc_rust/src/day03.rs
@@ -1,7 +1,20 @@
 use cxx::CxxString;
 
 fn get_rule(input: &[u32], index: u32) -> u32 {
-  input.iter().filter(|x| (x.clone() & (1 << index)) != 0).count() as u32
+  let counts = input.iter().filter(|x| (x.clone() & (1 << index)) != 0).count() as u32;
+  if counts >= input.len() as u32 - counts { 1 } else { 0 }
+}
+
+fn tree_decent(input: &[u32], line_length : u32, option : bool) -> u32 {
+  let mut input_copy = input.to_vec();
+  for i in (0..line_length).rev() {
+    let rule = get_rule(&input_copy, i as u32) ^ option as u32;
+    input_copy.retain(|x| (x >> i) & 1 == rule);
+    if input_copy.len() == 1 {
+      break;
+    }
+  }
+  input_copy[0]
 }
 
 pub fn day03functional(filename: &CxxString, part1: &mut u64, part2: &mut u64) {
@@ -14,7 +27,7 @@ pub fn day03functional(filename: &CxxString, part1: &mut u64, part2: &mut u64) {
     .map(|x| {
       // I don't find a proper to do that in "rust"
       if line_length == 0 {
-        line_length = x.chars().count()
+        line_length = x.chars().count() as u32
       };
       x
     })
@@ -25,34 +38,14 @@ pub fn day03functional(filename: &CxxString, part1: &mut u64, part2: &mut u64) {
 
   let mut gamma = 0;
   for i in 0..line_length as u32 {
-    gamma += if get_rule(&input_puzzle, i) > input_puzzle.len() as u32 / 2 {
-      1 << i
-    } else {
-      0
-    }
+    gamma += get_rule(&input_puzzle, i) << i;
   }
   let epsilon = (1 << line_length)-1 & !gamma;
 
-  *part1 = gamma * epsilon;
-  *part2 = 0;
+  *part1 = (gamma * epsilon) as u64;
+
+  let oxygen = tree_decent(&input_puzzle, line_length, false);
+  let co2 = tree_decent(&input_puzzle, line_length, true);
+
+  *part2 = (oxygen * co2) as u64;
 }
-
-
-// uint64_t treeDescent(const std::vector<uint16_t> &inputPuzzle, size_t lineLength, bool option) {
-//   uint64_t result = 0;
-//   auto begin = inputPuzzle.begin();
-//   auto end = inputPuzzle.end();
-//   // We reduce the search at each iteration by moving begin and end of the search
-//   for (size_t i = 0, shift = lineLength -1; i < lineLength; ++i, --shift) {
-//     auto pos = std::find_if(begin, end, [&](auto value) { return (value >> shift) & 1; });
-//     if (pos != end) {
-//       if ((pos - begin <= end - pos) ^ option) {
-//         begin = pos;
-//       } else {
-//         end = pos;
-//       }
-//       result = *pos;
-//     }
-//   }
-//   return result;
-// }
