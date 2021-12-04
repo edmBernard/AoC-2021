05f18a0ff5d72b59c6c592feb14d978f029bce87
edmbernard <bernard.erwan@gmail.com>
2021-12-04 22:31:34 +0100

    day04: use and operator and change type to bool

--- src/aoc_cxx/day04.cpp
+++ src/aoc_cxx/day04.cpp
@@ -4,7 +4,6 @@
 
 #include <range/v3/all.hpp>
 #include <spdlog/spdlog.h>
-#include <xsimd/xsimd.hpp>
 
 #include <charconv>
 #include <filesystem>
@@ -16,7 +15,6 @@
 
 namespace aoc {
 
-namespace xs = xsimd;
 namespace fs = std::filesystem;
 namespace rs = ranges;
 namespace rv = ranges::views;
@@ -50,87 +48,65 @@ inline std::vector<T> parse(const std::vector<std::string> &input, int base = 10
   return result;
 }
 
-template <int Dim>
-bool countOnRow(const std::array<uint8_t, Dim*Dim> &mark) {
-  std::array<uint8_t, Dim> counts{0};
+
+template <int Dim, typename T>
+void showBoard(const std::array<T, Dim*Dim> &mark) {
   for (size_t row = 0; row < Dim; ++row) {
     for (size_t col = 0; col < Dim; ++col) {
-      counts[row] += mark[row * Dim + col];
+      fmt::print("{} ", mark[row * Dim + col]);
     }
-    if (counts[row] == Dim)
-      return true;
+    fmt::print("\n");
   }
-  return false;
 }
 
-template <int Dim>
-bool countOnColumn(const std::array<uint8_t, Dim*Dim> &mark) {
-  std::array<uint8_t, Dim> counts{0};
+
+template <int Dim, typename T>
+void showVect(const std::array<T, Dim> &mark) {
   for (size_t col = 0; col < Dim; ++col) {
-    for (size_t row = 0; row < Dim; ++row) {
-      counts[col] += mark[row * Dim + col];
-    }
-    if (counts[col] == Dim)
-      return true;
+    fmt::print("{} ", mark[col]);
   }
-  return counts;
+  fmt::print("\n");
 }
 
+
 template <int Dim>
-bool detectWin(const std::array<uint8_t, Dim*Dim> &mark) {
-  std::array<uint8_t, Dim> countsRow{0};
-  std::array<uint8_t, Dim> countsCol{0};
+bool detectWin(const std::array<bool, Dim*Dim> &mark) {
+  std::array<bool, Dim> countsRow;
+  std::array<bool, Dim> countsCol;
+  countsRow.fill(true);
+  countsCol.fill(true);
   for (size_t row = 0; row < Dim; ++row) {
     for (size_t col = 0; col < Dim; ++col) {
-      const uint8_t value = mark[row * Dim + col];
-      countsCol[col] += value;
-      countsRow[row] += value;
+      const bool value = mark[row * Dim + col];
+      countsCol[col] &= value;
+      countsRow[row] &= value;
       // check if win on last row only
-      if (row == Dim - 1 && countsCol[col] == Dim)
+      if (row == Dim - 1 && countsCol[col])
         return true;
     }
     // check if win on last col
-    if (countsRow[row] == Dim)
+    if (countsRow[row])
       return true;
   }
+
   return false;
 }
 
 template <int Dim>
-bool detectWinSimd(const std::array<uint8_t, Dim*Dim> &mark) {
-  std::array<uint8_t, Dim> countsRow{0};
-  std::array<uint8_t, Dim> countsCol{0};
-  for (size_t row = 0; row < Dim; ++row) {
-    for (size_t col = 0; col < Dim; ++col) {
-      const uint8_t value = mark[row * Dim + col];
-      countsCol[col] += value;
-      countsRow[row] += value;
-      // check if win on last row only
-      if (row == Dim - 1 && countsCol[col] == Dim)
-        return true;
-    }
-    // check if win on last col
-    if (countsRow[row] == Dim)
-      return true;
-  }
-  return false;
-}
-
-template <int Dim>
-void updateBoard(const std::array<uint16_t, Dim*Dim> &board, std::array<uint8_t, Dim*Dim> &mark, uint16_t draw) {
+void updateBoard(const std::array<uint16_t, Dim*Dim> &board, std::array<bool, Dim*Dim> &mark, uint16_t draw) {
   auto pos = board.begin();
   while (true) {
     pos = std::find(pos, board.end(), draw);
     if (pos == board.end())
       break;
     const size_t index = pos - board.begin();
-    mark[index] = 1;
+    mark[index] = true;
     ++pos;
   }
 }
 
 template <int Dim>
-uint64_t getSum(const std::array<uint16_t, Dim*Dim> &board, std::array<uint8_t, Dim*Dim> &mark, uint16_t draw) {
+uint64_t getSum(const std::array<uint16_t, Dim*Dim> &board, std::array<bool, Dim*Dim> &mark, uint16_t draw) {
   uint64_t sumMarked = 0;
   for (size_t index = 0; index < board.size(); ++index) {
     if (mark[index] != 1) {
@@ -180,7 +156,7 @@ RegisterCommand day04("day04", {
       }
     }
 
-    std::vector<std::array<uint8_t, dim*dim>> mark(boards.size(), {0});
+    std::vector<std::array<bool, dim*dim>> mark(boards.size(), {0});
     std::vector<uint8_t> boardsThatWin(boards.size(), 0);
     uint64_t firstWinScore = 0;
     uint64_t lastWinScore = 0;
@@ -195,78 +171,6 @@ RegisterCommand day04("day04", {
         updateBoard<dim>(boards[i], mark[i], draw);
 
         const bool hasCompleted = detectWin<dim>(mark[i]);
-
-        // if the board win compute the score
-        if (hasCompleted) {
-          boardsThatWin[i] = 1;
-
-          if (firstWinScore == 0) {
-            firstWinScore = getSum<dim>(boards[i], mark[i], draw);
-          } else if (std::accumulate(boardsThatWin.begin(), boardsThatWin.end(), 0) == boards.size()) {
-            // check if it's the last winner we can't be first and last winner
-            return {firstWinScore, getSum<dim>(boards[i], mark[i], draw)};
-          }
-        }
-      }
-    }
-
-    return {0, 0};
-});
-
-
-RegisterCommand day04xsimd("day04,xsimd", {
-    { "input_day04.txt",       38913,   16836},
-    { "input_day04_test1.txt", 4512,    1924},
-  }, [](fs::path filename) -> std::tuple<uint64_t, uint64_t> {
-
-    std::ifstream infile(filename);
-    if (!infile.is_open()) {
-      throw std::runtime_error(fmt::format("File Not Found : {}", filename.string()));
-    }
-    constexpr int dim = 5;
-
-
-    std::string line;
-
-    getline(infile, line);
-    std::vector<uint16_t> numbersDrawn = parse<uint16_t>(split(line, ','));
-
-    std::vector<std::array<uint16_t, dim*dim>> boards;
-    std::array<uint16_t, dim*dim> temporaryBoard{0};
-
-    size_t lineInBoard = 0;
-    while (getline(infile, line)) {
-      if (line.empty())
-        continue;
-
-      std::istringstream instream(line);
-      std::istream_iterator<uint16_t> start(instream), end;
-      std::vector<uint16_t> boardLine(start, end);
-
-      std::copy_n(boardLine.begin(), dim, temporaryBoard.begin()+lineInBoard);
-      lineInBoard += dim;
-
-      if (lineInBoard >= dim*dim) {
-        boards.push_back(temporaryBoard);
-        lineInBoard = 0;
-      }
-    }
-
-    std::vector<std::array<uint8_t, dim*dim>> mark(boards.size(), {0});
-    std::vector<uint8_t> boardsThatWin(boards.size(), 0);
-    uint64_t firstWinScore = 0;
-    uint64_t lastWinScore = 0;
-
-    for (auto& draw : numbersDrawn) {
-      for (size_t i = 0; i < boards.size(); ++i) {
-        // skip board that already win
-        if (boardsThatWin[i] == 1)
-          continue;
-
-        // update mark in function of the drawn number
-        updateBoard<dim>(boards[i], mark[i], draw);
-
-        const bool hasCompleted = detectWinSimd<dim>(mark[i]);
 
         // if the board win compute the score
         if (hasCompleted) {
