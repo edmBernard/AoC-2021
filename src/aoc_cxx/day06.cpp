0ddba3506ccc0d2ed744a8ede6ac4cfd68a029d6
edmbernard <bernard.erwan@gmail.com>
2021-12-06 15:55:44 +0100

    day06: use class to precompute

--- src/aoc_cxx/day06.cpp
+++ src/aoc_cxx/day06.cpp
@@ -68,6 +68,51 @@ void showVect(const std::vector<T> &mark) {
   fmt::print("\n");
 }
 
+struct Engine {
+
+  Engine(size_t step) : step(step) {
+    preComputeResult(step);
+  }
+
+  uint64_t getFinalCount(const std::vector<uint8_t>& initialPopulation, uint16_t finalEpoch, uint16_t currentEpoch = 0) {
+    return computeTotal(initialPopulation, finalEpoch, currentEpoch);
+  }
+
+private:
+
+  uint64_t computeTotal(const std::vector<uint8_t>& initial, uint16_t finalEpoch, uint16_t currentEpoch) {
+    if (currentEpoch >= finalEpoch)
+      return initial.size();
+
+    uint64_t total = 0;
+    for (auto& p : initial) {
+      // spdlog::debug("final={}  current={}", finalEpoch, currentEpoch);
+      total += computeTotal(preComputedPopulation[p], finalEpoch, currentEpoch + step);
+    }
+    return total;
+  }
+
+  void preComputeResult(uint16_t epoch) {
+    for (int i = 0; i <= 8; ++i) {
+      // spdlog::debug("initial={}  epoch={}", initial, epoch);
+      std::vector<uint8_t> population{i};
+      for (uint16_t i = 0; i < epoch; ++i) {
+        uint64_t toAdd = 0;
+        std::transform(population.begin(), population.end(), population.begin(), [&toAdd](auto x) {
+            toAdd += (x == 0);
+            return x == 0 ? 6 : x - 1;
+          });
+        population.resize(population.size() + toAdd, 8);  // C++20
+      }
+      preComputedPopulation.push_back(population);
+    }
+  }
+
+  size_t step;
+  std::vector<std::vector<uint8_t>> preComputedPopulation;
+};
+
+
 std::vector<uint8_t> getFinalPopulation(uint8_t initial, uint16_t epoch) {
   std::vector<uint8_t> population{initial};
   for (uint16_t i = 0; i < epoch; ++i) {
@@ -81,6 +126,7 @@ std::vector<uint8_t> getFinalPopulation(uint8_t initial, uint16_t epoch) {
   return population;
 }
 
+
 uint64_t getCountPopulation(uint8_t initial, uint16_t epoch) {
   std::vector<uint8_t> population{initial};
   for (uint16_t i = 0; i < epoch; ++i) {
@@ -93,6 +139,7 @@ uint64_t getCountPopulation(uint8_t initial, uint16_t epoch) {
   }
   return population.size();
 }
+
 
 } // namespace
 
@@ -114,32 +161,13 @@ RegisterCommand day06("day06", {
       population = parse<uint8_t>(split(line, ','));
     }
 
-
     // part1
-    std::vector<uint64_t> preComputeCountPart1Step80;
-    for (int i = 0; i <=8; ++i) {
-      preComputeCountPart1Step80.push_back(getCountPopulation(i, 80));
-    }
-    uint64_t countPart1 = 0;
-    for (auto& v : population) {
-      countPart1 += preComputeCountPart1Step80[v];
-    }
+    Engine engine1(40);
+    uint64_t countPart1 = engine1.getFinalCount(population, 80);
 
     // part2
-    std::vector<std::vector<uint8_t>> preComputePopulationPart2Step128;
-    std::vector<uint64_t> preComputeCountPart2Step128;
-    for (int i = 0; i <=8; ++i) {
-      const std::vector<uint8_t> pop = getFinalPopulation(i, 128);
-      preComputeCountPart2Step128.push_back(pop.size());
-      preComputePopulationPart2Step128.push_back(pop);
-    }
-    uint64_t countPart2 = 0;
-    for (auto& v : population) {
-      auto pop = preComputePopulationPart2Step128[v];
-      for (auto& p : pop) {
-        countPart2 += preComputeCountPart2Step128[p];
-      }
-    }
+    Engine engine2(128);
+    uint64_t countPart2 = engine2.getFinalCount(population, 256);
 
     return {countPart1, countPart2};
 });
