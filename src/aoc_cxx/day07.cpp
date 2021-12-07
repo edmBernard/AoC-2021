8d8c280e16ea410a4b40818a36a0b4bdaafe3ee0
edmbernard <bernard.erwan@gmail.com>
2021-12-07 22:29:48 +0100

    day07: use dichotomy to

--- src/aoc_cxx/day07.cpp
+++ src/aoc_cxx/day07.cpp
@@ -63,6 +63,22 @@ void showVect(const std::vector<T> &mark) {
   fmt::print("\n");
 }
 
+template <typename T>
+uint64_t computePart1(const std::vector<T>& input, T position) {
+  uint64_t sum = 0;
+  for (auto& p : input) {
+    sum += std::abs(p - position);
+  }
+  return sum;
+}
+template <typename T>
+uint64_t computePart2(const std::vector<T>& input, T position) {
+  uint64_t sum = 0;
+  for (auto& p : input) {
+    sum += (std::abs(p - position) + 1) * std::abs(p - position) / 2;
+  }
+  return sum;
+}
 
 } // namespace
 
@@ -84,32 +100,44 @@ RegisterCommand day07("day07", {
       puzzleInput = parse<int32_t>(split(line, ','));
     }
 
-    // part1
     auto [pmin, pmax] = std::minmax_element(puzzleInput.begin(), puzzleInput.end());
 
-    int fuelPart1 = 0;
-    for (int i = *pmin; i < *pmax; ++i) {
+    // part1
+    // dichotomy method to reduce the search space
+    uint64_t fuelPart1 = 0;
+    {
+      std::pair<int, uint64_t> left{*pmin, computePart1(puzzleInput, *pmin)};
+      std::pair<int, uint64_t> right{*pmax, computePart1(puzzleInput, *pmax)};
 
-      float sum = 0;
-      for (auto& p : puzzleInput) {
-        sum += std::abs(p - i);
+      while (right.first - left.first > 1) {
+        int pos = (right.first + left.first) / 2;
+        uint64_t sum = computePart1(puzzleInput, pos);
+        if (left.second < right.second) {
+          right = {pos, sum};
+        } else {
+          left = {pos, sum};
+        }
       }
-      if (fuelPart1 == 0 || sum < fuelPart1) {
-        fuelPart1 = sum;
-      }
+      fuelPart1 = std::min(right.second, left.second);
     }
 
     // part2
-    int64_t fuelPart2 = 0;
-    for (int64_t i = *pmin; i < *pmax; ++i) {
+    // dichotomy method to reduce the search space
+    uint64_t fuelPart2 = 0;
+    {
+      std::pair<int, uint64_t> left{*pmin, computePart2(puzzleInput, *pmin)};
+      std::pair<int, uint64_t> right{*pmax, computePart2(puzzleInput, *pmax)};
 
-      int64_t sum = 0;
-      for (auto& p : puzzleInput) {
-        sum += (std::abs(p - i) + 1) * std::abs(p - i) / 2;
+      while (right.first - left.first > 1) {
+        int pos = (right.first + left.first) / 2;
+        uint64_t sum = computePart2(puzzleInput, pos);
+        if (left.second < right.second) {
+          right = {pos, sum};
+        } else {
+          left = {pos, sum};
+        }
       }
-      if (fuelPart2 == 0 || sum < fuelPart2) {
-        fuelPart2 = sum;
-      }
+      fuelPart2 = std::min(right.second, left.second);
     }
     return {fuelPart1, fuelPart2};
 });
